c3b409dde631f0508931e0a6cfb3004f3b72072c
687333 <687333@ELF7IT219027.ACDM.DS.SAIT.CA>
2018-10-01 08:01:15 -0600

    Worked on the session for lab 4.

--- src/java/Servlets/LoginServlet.java
+++ src/java/Servlets/LoginServlet.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servlets;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import Models.User;
+import Services.UserService;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -27,14 +19,38 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException 
     {
-           getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException 
     {
-  
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        if(username.equals(null) || username.equals("") ||
+                password.equals(null) || password.equals(""))
+        {
+            request.setAttribute("message", "please enter in both a username and a password");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            return;
+        }
+        
+        UserService userService = new UserService();
+        User user = userService.login(username, password);
+        
+        if(user == null)
+        {
+            request.setAttribute("message", "Invalid username or password... Please try again.");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
+        else{
+             HttpSession session = request.getSession();
+             session.setAttribute("user", user);
+             response.sendRedirect("Home");
+        }
     }
 
 }
